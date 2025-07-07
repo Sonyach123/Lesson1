7ce2072b363b8bb32e045c46378b3d686770d5be
Kuhii <tatianakugiy@gmail.com>
2025-07-07 16:22:34 +0300

    3commit

--- test2.js
+++ test2.js
@@ -1,3 +1,13 @@
-var message;
-message - 'Hello world';
+// "use strict"
+
+// var message;
+// message = "Hello world";
+// console.log(message);
+
+// const message = "Hello world!";
+// console.log(message);
+var message = "Hello";
+console.log(message);
+
+message = "other";
 console.log(message);
