17086562005ace4a25e1c7a632194b367995ec12
Kuhii <tatianakugiy@gmail.com>
2025-07-11 15:51:07 +0300

    new commit

---
+++ test.js
@@ -0,0 +1,24 @@
+var x;
+var n = null;
+var res = false | n || x || "prosto ryadok" || (x=5) | true;
+console.log(res);
+console.log(x);
+
+console.log(!true);
+console.log(!false);
+console.log(!"");
+console.log(!0);
+console.log(!null);
+console.log(!underfined);
+
+
+
+
+let birthday = 25;
+let discount;
+if (birthday ===25) {
+    discount = "30%";
+} else{
+    discount ="10%";
+}
+console.log(discount);
