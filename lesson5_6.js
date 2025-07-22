2d03aa68835c6c861c4b1d316d2b0e48e45d3f4d
Kuhii <tatianakugiy@gmail.com>
2025-07-22 19:19:06 +0300

    lesson8-6

--- lesson5_6.js
+++ lesson5_6.js
@@ -1,6 +1,28 @@
-function some(arg1, arg2){
-console.log(arg1,arg2);
-console.log("it works!")
-}
+// function some(arg1, arg2){
+// console.log(arg1,arg2);
+// console.log("it works!")
+// }
 
-some(3,7);
+// some(3,7);
+
+// function squarecalculation(){
+//     let a=5
+// console.log(a*a);
+// }
+
+// squarecalculation();
+
+// let b = function(){
+//     console.log (2*2);
+// ;}
+// console.log(b)
+
+
+let trainfunction = function(){
+    console.log("train function works");
+}
+trainfunction();
+
+function squarecalculation1(arg1,arg2){
+    
+}
