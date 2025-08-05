e601f2a10d5704a33b7933c5e93f92a0bb8f7d61
Kuhii <tatianakugiy@gmail.com>
2025-08-05 13:38:17 +0300

    during lesson5

--- lesson5_6.js
+++ lesson5_6.js
@@ -17,7 +17,7 @@
 // ;}
 // console.log(b)
 
-
+pow();
 let trainfunction = function(){
     console.log("train function works");
 }
@@ -26,3 +26,34 @@ trainfunction();
 function squarecalculation1(arg1,arg2){
     
 }
+pow();
+function pow(){
+    console.log("hi from function")
+}
+pow();
+
+var ext = "outside text";
+
+function lessonexample(){
+    var inner = "inner function text";
+    console.log("function sees outside: " + ext);
+    console.log("function sees inside: " + inner);
+}
+lessonexample();
+
+function varchangessduringfunction(){
+    console.log("before change: " + myVar);
+    myVar = "I am this";
+    console.log("after change: " + myVar);
+    var myVar;
+} 
+varchangessduringfunction();
+
+function hometask(base,limit){
+    for (let x = 0; x < limit; x++) {
+     base *= base;
+     console.log("inside cycle x = " + x);   
+    }
+    console.log("outside cycle x = " + x);
+}
+hometask(2, 5);
